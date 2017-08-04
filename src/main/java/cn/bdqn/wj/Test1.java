7b07b004b7155d4fd0747060276d6a271281b98d
544513099 <544513099@qq.com>
2017-08-04 19:10:22 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/wj/Test1.java
@@ -0,0 +1,14 @@
+package cn.bdqn.wj;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/4 0004.
+ */
+public class Test1 {
+@Test
+public void test(){
+    System.out.println("这是啥东西");
+}
+
+}
